7e5449f9740ef6fda68ed8f7b552a6a34c3baa3b
MeriamErraji <meriam.erraji@dauphine.eu>
2025-02-28 16:06:24 +0100

    Davoust_Patrimoine.py
    Mise à jour des stats du portefeuille Arbitré

--- Davoust_Patrimoine.py
+++ Davoust_Patrimoine.py
@@ -22,14 +22,17 @@ def load_data():
     indicateur_benchmark = pd.read_csv(os.path.join(data_folder, "indicateur_benchmark.csv"), index_col=0, parse_dates=True)
     eur_price_matrix = pd.read_csv(os.path.join(data_folder, "eur_price_matrix.csv"), index_col=0, parse_dates=True)
     metriques_ptf_initial = pd.read_excel(os.path.join(metrics_folder, "metriques_ptf_initial.xlsx"))
+    indicateur_offensif = pd.read_csv(os.path.join(data_folder, "indicateurs_offensifs.csv"), index_col=0, parse_dates=True)
+    metriques_ptf_arbitre = pd.read_excel(os.path.join(metrics_folder, "metriques_ptf_arbitre.xlsx"))
+
     
     # Données du portefeuille arbitré
     df_poids_offensif = pd.read_csv(os.path.join(data_folder, "df_poids_offensif.csv"))
     indicateurs_offensifs = pd.read_csv(os.path.join(data_folder, "indicateurs_offensifs.csv"), index_col=0, parse_dates=True)
     
-    return df_weight, indicateur_ptf, indicateur_benchmark, eur_price_matrix, metriques_ptf_initial, df_poids_offensif, indicateurs_offensifs
+    return df_weight, indicateur_ptf, indicateur_benchmark, eur_price_matrix, metriques_ptf_initial, indicateur_offensif, metriques_ptf_arbitre, indicateurs_offensifs
 
-df_weight, indicateur_ptf, indicateur_benchmark, eur_price_matrix, metriques_ptf_initial, df_poids_offensif, indicateurs_offensifs = load_data()
+df_weight, indicateur_ptf, indicateur_benchmark, eur_price_matrix, metriques_ptf_initial, indicateurs_offensif, metriques_ptf_arbitre, indicateurs_offensifs= load_data()
 
 # Après le chargement des données, ajoutez :
 print("Colonnes disponibles dans indicateurs_offensifs:")
